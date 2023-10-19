fe217d6f45cd57746530bcf7dedf25209df17118
shadinmhd <108604275+shadinmhd@users.noreply.github.com>
2023-10-19 12:28:14 +0530

    removed transporter in sockets

--- client/src/services/Socket.tsx
+++ client/src/services/Socket.tsx
@@ -2,8 +2,6 @@ import { io } from "socket.io-client";
 
 const ENDPOINT = import.meta.env.VITE_BASEURL 
 
-const socket = io(ENDPOINT, {
-    transports: ["websocket"]
-});
+const socket = io(ENDPOINT, {});
 
 export default socket
